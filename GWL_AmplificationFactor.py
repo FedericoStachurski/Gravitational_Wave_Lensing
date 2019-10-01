550cbc32397c24055aa792761e379fb7a6787d79
FedericoStachurski <rico.stachurski@hotmail.com>
2019-10-01 16:02:04 +0100

    Amplification Factor and Phase Factor

--- GWL_AmplificationFactor.py
+++ GWL_AmplificationFactor.py
@@ -15,7 +15,7 @@ import math
 from scipy.special import gamma, factorial, hyp1f1
 import cmath
 from mpmath import *
-
+path="Plots/"
 #%% Set parameters 
 w = np.linspace( 0.001, 100, 1000) #frequency w = 8pi * M * f
 y = 0.1    # source position
@@ -64,12 +64,23 @@ def AmplificationFactor(y):
     Exp_L_F = np.multiply(Exp_L, F) #Result (Amplification Factor)
     return np.absolute(Exp_L_F)
 
-#%%
-plt.plot(w, AmplificationFactor(0.1), 'w')
-plt.grid(True)
+#%% Plot and save figure
+fig = plt.figure()
+ax = plt.subplot(111)
+ax.plot(w, AmplificationFactor(0.01),'c',label='$y = 0.01$')
+ax.plot(w, AmplificationFactor(0.05),'b' ,label='$y = 0.05$')
+ax.plot(w, AmplificationFactor(0.1), 'w',label='$y = 0.1$')
+ax.plot(w, AmplificationFactor(0.3), 'y',label='$y = 0.3$')
+ax.plot(w, AmplificationFactor(1.0), 'r',label='$y = 1.0$')
+ax.plot(w, AmplificationFactor(3.0), 'g',label='$y = 3.0$')
+
+ax.legend()
+matplotlib.pyplot.grid(True, which="both", alpha=0.2)
+plt.ylabel('|F|')
+plt.xlabel('w')
 plt.xscale('log')
 plt.yscale('log')
-plt.ylim([0.1, 10])
-
+plt.ylim([0.1, 100])
+fig.savefig(path+'AmplFactor_PM.png', dpi=1200, bbox_inches = "tight")
 
 #%%
